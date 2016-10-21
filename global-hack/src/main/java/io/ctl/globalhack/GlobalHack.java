9ab07a92046c145626dc35da6446522ad7a309ce
SeanRobb <sean.robb@centurylink.com>
2016-10-21 15:22:40 -0500

    initial

---
+++ global-hack/src/main/java/io/ctl/globalhack/GlobalHack.java
@@ -0,0 +1,13 @@
+package io.ctl.globalhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalHack {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalHack.class, args);
+    }
+
+}
