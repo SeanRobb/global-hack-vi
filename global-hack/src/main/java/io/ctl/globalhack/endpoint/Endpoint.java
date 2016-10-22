6efb6f78b3f862a4d243117bcb9ed827ef7baee6
SeanRobb <sean.robb@centurylink.com>
2016-10-22 17:59:22 -0500

    added income and benefits

--- global-hack/src/main/java/io/ctl/globalhack/endpoint/Endpoint.java
+++ global-hack/src/main/java/io/ctl/globalhack/endpoint/Endpoint.java
@@ -57,14 +57,6 @@ public class Endpoint {
     }
 
 
-//    @RequestMapping(method = RequestMethod.PUT, path = "/provider")
-//    public Provider createNeedService(@RequestBody Provider provider) {
-//        log.info("Creating Need Service... {}", provider);
-//        if (provider.getId() == null) {
-//            provider.setId(UUID.randomUUID().toString());
-//        }
-//        return providerRepository.save(provider);
-//    }
 
     @RequestMapping(method = RequestMethod.PUT, path = "/provider")
     public Provider createNeedService(@RequestBody UiProvider uiProvider) {
