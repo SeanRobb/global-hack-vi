d1c3c11d68a994cd3049c410da3a36f9d1e2ce16
kevin <kjwilde@gmail.com>
2016-10-22 20:15:53 -0500

    Added created date and update date.

--- global-hack/src/main/java/io/ctl/globalhack/service/PersonInNeedService.java
+++ global-hack/src/main/java/io/ctl/globalhack/service/PersonInNeedService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -71,6 +72,9 @@ public class PersonInNeedService {
         if (personInNeed.getId() == null) {
             personInNeed.setId(UUID.randomUUID().toString());
         }
+
+        personInNeed.setDateCreated(new Date());
+        personInNeed.setDateUpdated(new Date());
 
         if (fromPhoneNumber != null) {
             personInNeed.setPhoneNumbers(Arrays.asList(fromPhoneNumber));
