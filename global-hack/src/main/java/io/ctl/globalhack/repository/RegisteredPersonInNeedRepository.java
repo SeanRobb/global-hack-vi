f59e7b4cbb23fccbaae1c4c07336cd334873278a
SeanRobb <sean.robb@centurylink.com>
2016-10-22 19:33:48 -0500

    add phone number lists

--- global-hack/src/main/java/io/ctl/globalhack/repository/RegisteredPersonInNeedRepository.java
+++ global-hack/src/main/java/io/ctl/globalhack/repository/RegisteredPersonInNeedRepository.java
@@ -1,8 +1,6 @@
 package io.ctl.globalhack.repository;
 
-import io.ctl.globalhack.common.PersonInNeed;
 import io.ctl.globalhack.common.RegisteredPersonInNeed;
-import io.ctl.globalhack.common.person.info.veteran.VeteranInfo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
@@ -11,9 +9,6 @@ import java.util.List;
  * Created by sean on 10/22/16.
  */
 public interface RegisteredPersonInNeedRepository extends MongoRepository<RegisteredPersonInNeed, String> {
-    List<RegisteredPersonInNeed> findByVeteranInfoNotExists();
-    List<RegisteredPersonInNeed> findByVeteranInfoExists();
-
-    List<PersonInNeed> findByPhoneNumbersIn(String phoneNumber);
+    List<RegisteredPersonInNeed> findByPersonInNeedPhoneNumbersIn(String phoneNumber);
 
 }
