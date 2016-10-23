df69684f402114ef02f701778ea9ffb92eccdab3
SeanRobb <sean.robb@centurylink.com>
2016-10-22 20:20:49 -0500

    services endpoint

--- global-hack/src/main/java/io/ctl/globalhack/repository/ProviderRepository.java
+++ global-hack/src/main/java/io/ctl/globalhack/repository/ProviderRepository.java
@@ -10,8 +10,6 @@ public interface ProviderRepository extends MongoRepository<Provider, String> {
 
     List<Provider> findByAvailableNameIn(String serviceName);
 
-    List<Provider> findDistinctOfferName();
-
     List<Provider> findByAvailableNameInAndAvailableAmountGreaterThanEqual(String serviceName, BigDecimal amount);
 
     List<Provider> findByAddressZipCode(String zipCode);
